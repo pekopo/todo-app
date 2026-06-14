ed95cc09e19e3a91f5c8966dec18d82d1b6d209d
pekopo <masaki.toyota54@gmail.com>
2026-06-14 14:45:58 +0900

    #2-ReactHello作成

---
+++ todo-web/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hello World from React!</h1>
+      <p>TODO App frontend is running.</p>
+    </>
+  );
+}
+
+export default App
